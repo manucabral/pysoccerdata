30dff6fea522a8a6b2dfcfd537fdf518f40efefe
Manu <mandrescabral@gmail.com>
2025-02-27 17:16:41 -0300

    feat: add win probability to MatchTeam and update parse_match_team function

--- pysoccerdata/match/match_team.py
+++ pysoccerdata/match/match_team.py
@@ -15,11 +15,19 @@ class MatchTeam:
     _id: int = field(metadata={"doc": "Unique id"})
     name: str = field(metadata={"doc": "Team name"})
     score: int = field(metadata={"doc": "Team score"})
+    win_probability: int = field(default=0, metadata={"doc": "Win probability"})
 
 
-def parse_match_team(data: typing.Dict, score_key: str, team_key: str) -> MatchTeam:
-    return MatchTeam(
+def parse_match_team(
+    data: typing.Dict, score_key: str, team_key: str, win_prob: dict
+) -> MatchTeam:
+    details = MatchTeam(
         _id=data.get(team_key).get("id"),
         name=data.get(team_key).get("shortName"),
         score=data.get(score_key).get("current"),
     )
+    if win_prob:
+        details.win_probability = win_prob.get(
+            "homeWin" if team_key == "homeTeam" else "awayWin"
+        )
+    return details
