4e7d70d4d36696ac32e568d57f0157ec29d862c9
Manu <mandrescabral@gmail.com>
2025-02-27 15:10:37 -0300

    feat: add MatchCard field to MatchDetails class

--- pysoccerdata/match/match_details.py
+++ pysoccerdata/match/match_details.py
@@ -5,6 +5,7 @@ Contains the Match class.
 from dataclasses import dataclass, field
 from .match_team import MatchTeam
 from .match_stats import MatchStats
+from .match_cards import MatchCard
 
 
 @dataclass
@@ -17,3 +18,4 @@ class MatchDetails:
     home_team: MatchTeam = field(metadata={"doc": "Home team"})
     away_team: MatchTeam = field(metadata={"doc": "Away team"})
     stats: MatchStats = field(metadata={"doc": "Match stats"})
+    cards: MatchCard = field(metadata={"doc": "Match cards"})
