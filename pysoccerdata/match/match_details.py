8a5ebf05e652dad5e3d1ea7ca44ffc61af3f9985
Manu <mandrescabral@gmail.com>
2025-02-27 17:03:14 -0300

    feat: update MatchDetails to support multiple MatchCards and MatchGoals

--- pysoccerdata/match/match_details.py
+++ pysoccerdata/match/match_details.py
@@ -1,11 +1,12 @@
 """
 Contains the Match class.
 """
-
+from typing import List
 from dataclasses import dataclass, field
 from .match_team import MatchTeam
 from .match_stats import MatchStats
 from .match_cards import MatchCard
+from .match_goal import MatchGoal
 
 
 @dataclass
@@ -18,4 +19,5 @@ class MatchDetails:
     home_team: MatchTeam = field(metadata={"doc": "Home team"})
     away_team: MatchTeam = field(metadata={"doc": "Away team"})
     stats: MatchStats = field(metadata={"doc": "Match stats"})
-    cards: MatchCard = field(metadata={"doc": "Match cards"})
+    cards: List[MatchCard] = field(metadata={"doc": "Match cards"})
+    goals: List[MatchGoal] = field(metadata={"doc": "Match goals"})
