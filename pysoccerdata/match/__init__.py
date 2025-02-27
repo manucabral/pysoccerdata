e38813d4d7ed6c5048fc182c47141b6f2b8f9596
Manu <mandrescabral@gmail.com>
2025-02-27 15:10:24 -0300

    feat: add match incident and card parsing classes to match module

--- pysoccerdata/match/__init__.py
+++ pysoccerdata/match/__init__.py
@@ -2,8 +2,10 @@ from .match_details import MatchDetails
 from .match_summary import MatchSummary
 from .match_team import parse_match_team
 from .match_status import MatchStatus
-from .match_stats import parse_match_stats
+from .match_stats import parse_match_stats, MatchStats, StatMatchItem
 from .match_utils import filter_by
+from .match_incident import IncidentType
+from .match_cards import parse_match_cards, MatchCard, MatchCardPlayer
 
 __all__ = [
     "MatchDetails",
@@ -12,4 +14,10 @@ __all__ = [
     "MatchStatus",
     "parse_match_stats",
     "filter_by",
+    "IncidentType",
+    "parse_match_cards",
+    "MatchCard",
+    "MatchCardPlayer",
+    "StatMatchItem",
+    "MatchStats",
 ]
