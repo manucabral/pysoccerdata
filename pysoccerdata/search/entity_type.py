d8336b6fa950920022d83073a0bd3a294d98f878
Manu <mandrescabral@gmail.com>
2025-02-28 00:49:46 -0300

    feat: add EntityType enum to define various entity types for improved categorization

---
+++ pysoccerdata/search/entity_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class EntityType(Enum):
+    """
+    Enum with all entity types.
+    """
+    
+    ALL = "all"
+    TEAM = "teams"
+    PLAYER = "player-team-persons"
+    TOURNAMENT = "unique-tournaments"
+    MATCH = "events"
