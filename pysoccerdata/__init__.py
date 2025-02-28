7a650576d18c91774fba8231e90ed99475b4184f
Manu <mandrescabral@gmail.com>
2025-02-28 00:49:24 -0300

    feat: expand module's __all__ to include MatchSummary, MatchStats, MatchDetails, TournamentCategory, and EntityType for improved accessibility

--- pysoccerdata/__init__.py
+++ pysoccerdata/__init__.py
@@ -1,5 +1,16 @@
 from .client import Client
 from .match import filter_by
-from .match import MatchStatus
+from .match import MatchStatus, MatchSummary, MatchStats, MatchDetails
+from .tournament import TournamentCategory
+from .search import EntityType
 
-__all__ = ["Client", "filter_by", "MatchStatus"]
+__all__ = [
+    "Client",
+    "filter_by",
+    "MatchStatus",
+    "TournamentCategory",
+    "EntityType",
+    "MatchSummary",
+    "MatchStats",
+    "MatchDetails",
+]
